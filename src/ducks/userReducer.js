1e8dbbb0a4c7673263e2acbc95fcfa8151eb7281
Mark Carey <markallencarey@gmail.com>
2020-12-10 16:18:06 -0700

    finished homework

---
+++ src/ducks/userReducer.js
@@ -0,0 +1,33 @@
+import reducer from "./budgetReducer"
+import axios from 'axios'
+
+const initialState = {
+  email: null,
+  firstName: null,
+  lastName: null
+}
+
+const REQUEST_USER_DATA = 'REQUEST_USER_DATA'
+
+export const requestUserData = () => {
+  let data = axios.get('/auth/user-data').then(res => res.data)
+    return {
+      type: REQUEST_USER_DATA,
+      payload: data
+    }
+}
+
+export default function userReducer (state=initialState, action) {
+  switch (action.type) {
+    case REQUEST_USER_DATA:
+      return {
+        email: action.payload.user,
+        firstName: action.payload.firstName,
+        lastName: action.payload.las
+      }
+    default:
+      return state
+
+  }
+  
+}
